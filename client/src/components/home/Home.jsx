2af3183f96ea4cebea7a52b30ed74ec6540490f5
Adeline <adeline.sire@gmail.com>
2021-12-03 14:05:19 +0100

    Adds ShowList to Home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+import ShowsList from '../shows/ShowsList';
+
+import { getShows } from '../../services/api';
+
 import './Home.scss';
 
 const Home = () => {
-	return <div className='home'>Accueil</div>;
+	const [shows, setShows] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		getShows().then((response) => {
+			setShows(response);
+			setIsLoading(false);
+		});
+	}, []);
+	return (
+		<div className='home'>
+			{isLoading ? (
+				<p>Chargement</p>
+			) : (
+				<>
+					<p>Sélectionner une représentation pour réserver</p>
+					<ShowsList shows={shows} />
+				</>
+			)}
+		</div>
+	);
 };
 
 export default Home;
